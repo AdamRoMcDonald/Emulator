b63069b03b04def254e526f35744ea24f224d6ec
Adam McDonald <117361557+AdamRoMcDonald@users.noreply.github.com>
2024-09-21 01:11:50 +0000

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,111 +1,141 @@
 #include <iostream>
-#include <unistd.h>   // For fork(), exec(), chdir()
-#include <sys/wait.h> // For wait()
-#include <dirent.h>   // For opendir(), readdir()
+#include <unistd.h>   
+#include <sys/wait.h> 
+#include <dirent.h>   
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <cstring>    // For strcmp(), strtok()
-#include <fstream>    // For viewing files
+#include <cstring>    
+#include <fstream>  
+#include <string.h>
+
+//1. CD 
+
+
+
 
 void CD(const char* filePath) {
+    //In c, successful processes will often return 0
+    //Here, if chdir (the c keyword to use Linux's "cd" command) is unsuccessful it will print an error message-
+    // - just like a Linux shell would. 
     if (chdir(filePath) != 0) {
-        perror("chdir() error");
+        fprintf(stderr, "bash: cd: %s: No such file or directory\n", filePath);
     }
 }
 
 void LS() {
-    char cwd[1024];
+    //cwd means current working directory
+    char cwd[512];
     if (getcwd(cwd, sizeof(cwd)) != nullptr) {
         std::cout << cwd << std::endl;
-
+        //Here we check to see if our directory has any contents. 
         DIR* curDir = opendir(cwd);
         if (curDir == nullptr) {
-            perror("opendir() error");
             return;
         }
-
+        //Here we read through the contents of our directory. 
         struct dirent* FILE;
         while ((FILE = readdir(curDir)) != nullptr) {
             std::cout << FILE->d_name << std::endl;
         }
         closedir(curDir);
-    } else {
-        perror("getcwd() error");
-    }
+    } 
 }
 
-void PS() {
-    if (fork() == 0) {
-        execl("/bin/ps", "ps", "-e", nullptr);
-        perror("execl() error");
-        exit(1);
-    } else {
-        wait(nullptr);
-    }
-}
 
 void VI() {
+    //Here we can see a new process being forked.
+    //A child fork() will be equal to zero, indicating that it is a new process ready to run
     if (fork() == 0) {
+        //Here, execl replaces the CURRENT process image with a new image specified by the path, thus opening VI.
         execl("/usr/bin/vi", "vi", nullptr);
-        perror("execl() error");
         exit(1);
     } else {
         wait(nullptr);
     }
 }
 
-void CAT(const char* filename) {
-    std::ifstream file(filename);
+
+//Deleting file method
+
+void RM(const char* fileName) {
+    //Here we have another example of a successful process returning 0. 
+    if (remove(fileName) != 0) {
+        fprintf(stderr, "bash: cd: %s: No such file or directory\n", fileName);
+    } 
+}
+
+//Viewing file method
+
+void CAT(const char* fileName) {
+    //ifstream is a type of streamable class which allows us to operate on or "stream" the contents of a file. 
+    std::ifstream file(fileName);
+    //If file is false then the file doesn't exist. 
     if (!file) {
-        std::cerr << "Error: File not found or could not be opened!" << std::endl;
+        fprintf(stderr, "bash: cd: %s: No such file or directory\n", fileName);
         return;
     }
-
-    std::string line;
-    while (std::getline(file, line)) {
-        std::cout << line << std::endl;
+    
+    std::string singleL;
+    //Here this loop gets each line from the streamable file object, stores them temporarily in singleL and prints.
+    while (std::getline(file, singleL)) {
+        std::cout << singleL << std::endl;
     }
+    //Closing file
     file.close();
 }
 
-void RM(const char* filename) {
-    if (remove(filename) != 0) {
-        perror("Error deleting file");
+//View running processes method
+
+void PS() {
+    if (fork() == 0) {
+        //Here we again see a new child process replacing the current image with a new one found at the path.
+        execl("/bin/ps","ps","-e", nullptr);
+        exit(1);
     } else {
-        std::cout << "File deleted successfully!" << std::endl;
+    
+        wait(nullptr);
     }
 }
 
+
+//main driver method
+
 int main() {
-    char input[1024];
+    char input[512];
     char* args[64];
 
     while (true) {
         // print prompt
-        char cwd[1024];
+        char cwd[512];
         getcwd(cwd, sizeof(cwd));
         std::cout << cwd << " $ ";
 
-        // read input hopefull
+        // read input hopefully
+        //.getline() stores the line in "input"
         std::cin.getline(input, sizeof(input));
-
+          
+        //strtok divides input up into substrings, dividing where there are spaces.
+        //So "cd test.txt" would break up "cd" and "test.txt" into substrings so they can be used.
         char* token = strtok(input, " ");
         int i = 0;
+        
+        //This here adds these substrings into args
         while (token != nullptr) {
             args[i++] = token;
             token = strtok(nullptr, " ");
         }
         args[i] = nullptr;
 
-        // Handle commands
+        // handle user input commands, tried to make it a switch but it wouldn't work
+        //strcmp compares two strings, if they are identical it returns 0
         if (strcmp(args[0], "cd") == 0) {
             CD(args[1]);
         } else if (strcmp(args[0], "ls") == 0) {
             LS();
-        } else if (strcmp(args[0], "ps") == 0) {
-            PS();
         } else if (strcmp(args[0], "vi") == 0) {
             VI();
+        } else if (strcmp(args[0], "ps") == 0) {
+            PS();
         } else if (strcmp(args[0], "cat") == 0) {
             CAT(args[1]);
         } else if (strcmp(args[0], "rm") == 0) {
@@ -113,7 +143,7 @@ int main() {
         } else if (strcmp(args[0], "exit") == 0) {
             break;
         } else {
-            std::cerr << "Unknown command: " << args[0] << std::endl;
+            std::cerr << args[0] << ": command not found " << std::endl;
         }
     }
 
